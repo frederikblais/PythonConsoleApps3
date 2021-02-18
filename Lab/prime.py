f7a586b56708c4a807f280b7d3c0a22678d5395c
frederikblais <frederik.blais22@gmail.com>
2021-02-18 15:31:07 -0500

    prime

--- Lab/prime.py
+++ Lab/prime.py
@@ -1,6 +1,6 @@
 
 line = '------------------------------------------'
-title = '|         PYRAMID CALCULATOR             |'
+title = '|          PRIME CALCULATOR             |'
 
 # Header
 print (line)
@@ -9,17 +9,47 @@ print (line)
 
 # Ask for input of int n
 n = int(input('Enter a integer greater than 2: '))
+state = False
 print()
 
-if(n > 2):
-    def is_prime(n):
-        print('ok')
-
-
-
-
-
-
+def is_prime(n):
+        # prime numbers are greater than 1
+    if n > 1:
+    # check for factors
+        for i in range(2,n):
+            if (n % i) == 0:
+                state = False
+                print(state)
+                print()
+                break
+            else:
+                state = True
+                print(state)
+                print()
         
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+        print(n,"is not a prime number")
+
+def output():
+
+    for i in range (2,100,1):
+        num = 2;
+        # check for factors
+        for i in range(2,num):
+            if (num % i) == 0:
+                print(num, ' is not prime')
+                num +=1
+                break
+            else:
+                print(num, ' is prime')
+                num += 1
+
+if(n > 2):
+    is_prime(n)
+    output()
+
+
 else:
     print('You must enter a value greater than 2.')
